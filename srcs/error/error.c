892169deb508f4a6ad4a8820da3f818814f5f509
Max Govindaraju <mgovinda@k2r3p6.42mulhouse.fr>
2024-08-13 18:01:18 +0200

    created t_ray struc and created and error file

--- srcs/error/error.c
+++ srcs/error/error.c
@@ -1,49 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   types.h                                            :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/11 22:29:40 by wkeiser           #+#    #+#             */
-/*   Updated: 2024/08/13 17:58:32 by mgovinda         ###   ########.fr       */
+/*   Created: 2024/08/13 17:59:21 by mgovinda          #+#    #+#             */
+/*   Updated: 2024/08/13 18:00:11 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TYPES_H
-# define TYPES_H
+#include "../includes/cub3d.h"
 
-typedef struct s_map
+void	exit_exclaim(char *string)
 {
-	char	*line;
-	char	*n_path;
-	char	*s_path;
-	char	*w_path;
-	char	*e_path;
-	char	*ccolor;
-	char	*fcolor;
-	int		fd;
-}			t_map;
-
-/*
-typedef struct s_img
-{
-
-}				t_img;
-*/
-
-typedef struct s_data
-{
-	t_map	*map;
-	int		floorcolor;
-	int		ceilingcolor;
-}				t_data;
-
-typedef struct s_ray
-{
-	double	ray_agl;
-	double	distance;
-	int		flag;
-}				t_ray;
-
-#endif
+	ft_printf(1, "%s", string);
+	/*
+	Je n ai pas inclu de free car ajout du garbage collector pour tout free ?
+	Fonction a add autre part que parsing eventuellement / repertoire check ou err ?
+	*/
+	wclear();
+	exit (1);
+}
