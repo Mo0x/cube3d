ccab40697386158bc48cde09fa5f5a03f2a0670c
Will <william.keiser67@gmail.com>
2024-08-29 10:57:04 +0200

    creating minimap folder and define .h

--- srcs/main.c
+++ srcs/main.c
@@ -28,12 +28,6 @@ int	main(int argc, char **argv)
 	if (!c3d->player)
 		exit_exclaim("c3d->player malloc failed\n");
 	parsing(c3d->map, argv, argc, c3d);
-	printf("North path :%s\n", c3d->map->n_path);
-	printf("S path :%s\n", c3d->map->s_path);
-	printf("W path :%s\n", c3d->map->w_path);
-	printf("E path :%s\n", c3d->map->e_path);
-	printf("Converted ceiling color: 0x%X\n", c3d->floorcolor); //testing hexa conversion color
-	printf("Converted floor color: 0x%X\n", c3d->ceilingcolor); //testing hexa conversion color
 	/* ft_start_game(c3d)*/
 	wclear(); /* <---- a mettre a chaque fin dexec pour clear les allocs*/
 	return (0);
