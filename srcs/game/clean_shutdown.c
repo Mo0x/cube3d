29c5bbe5966a75cfabb87013ea97082cf56c1202
Max Govindaraju <mgovinda@k2r2p2.42mulhouse.fr>
2024-09-17 18:33:23 +0200

    created clean escape when ESC

--- srcs/game/clean_shutdown.c
+++ srcs/game/clean_shutdown.c
@@ -6,7 +6,7 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:56:20 by mgovinda          #+#    #+#             */
-/*   Updated: 2024/09/17 18:14:20 by mgovinda         ###   ########.fr       */
+/*   Updated: 2024/09/17 18:32:47 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ void	ft_escape(mlx_key_data_t keydata, void *c4d)
 
 void	ft_clean_shutdown(t_data *c3d)
 {
-	wclear();
 	/*mlx_delete_texture here*/
 	if (c3d->mlx)
 		mlx_terminate(c3d->mlx);
+	wclear();
 }
